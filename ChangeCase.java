ed105b220446cba0fb9114f496b12394e67e8afb
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-06-01 23:34:40 +0530

    Add files via upload

---
+++ ChangeCase.java
@@ -0,0 +1,15 @@
+//4. Convert Uppercase to Lowercase
+//Input: "JAVA PROGRAM"
+//Output: "java program"
+public class ChangeCase {
+	public static void main(String[] args) {
+
+		String str = "JAVA PROGRAM";
+		String str2=str.toLowerCase();
+		System.out.println(str2);
+		
+		
+		
+		
+	}
+}
